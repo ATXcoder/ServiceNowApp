b51cb80169b0ef84318b5df1fe8ad48c37e94360
ATXCoder <tsloan82@gmail.com>
2015-01-23 14:33:22 -0600

    - Created login layout - Created styles for login layout

---
+++ app/src/main/java/com/groundupcoding/servicenow/LoginActivity.java
@@ -0,0 +1,51 @@
+package com.groundupcoding.servicenow;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+
+public class LoginActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText username = (EditText)findViewById(R.id.userName);
+        EditText password = (EditText)findViewById(R.id.password);
+
+        String userName = Settings.Read(this, "pref_username");
+
+        // Check for saved credentials
+        if(userName != "username")
+        {
+            username.setText(userName);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
